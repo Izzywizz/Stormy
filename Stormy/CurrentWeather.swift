8842a1c89ace9e889c0a5193f5e1b6334913f2e9
izzywizz <hello@izzyali.co.uk>
2017-06-29 15:02:39 +0100

    Initial Commit

---
+++ Stormy/CurrentWeather.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Izzy Ali on 29/06/2017.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+
+struct CurrentWeather {
+    let temperature: Double
+    let humidity: Double
+    let precipitationProbability: Double
+    let summary: String
+    let icon: String    
+}
